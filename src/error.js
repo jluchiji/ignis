1b5db78937696a3b7df6a6b06d1fca7159be0072
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-08-17 17:07:45 -0400

    Setting up YAML loading and error emission

---
+++ src/error.js
@@ -0,0 +1,74 @@
+/**
+ * error.js
+ *
+ * @author  Denis Luchkin-Zhou <denis@ricepo.com>
+ * @license MIT
+ */
+
+import _           from 'lodash';
+
+/**
+ * IgnisError
+ *
+ * @description    Error arising within Ignis.js that should be reported back
+ *                 to the client via an HTTP response.
+ */
+export default class IgnisError extends Error {
+
+  constructor(status, message, details) {
+    super(message);
+
+    this.name    = 'IgnisError';
+    this.status  = status;
+    this.message = message;
+    this.details = details;
+
+    if (details && details.sensitive) { this.sensitive = true; }
+  }
+
+
+  /**
+   * .panic(3)
+   *
+   * @static
+   * @access                   public
+   * @description              Throws an IgnisError.
+   * @param        {status}    Status code to report to the client.
+   * @param        {message}   Human-readable message to report.
+   * @param        {details}   Additional error details and options.
+   * @throws       {IgnisError}
+   */
+  static panic(status, message, details) {
+    throw new IgnisError(status, message, details);
+  }
+
+  /**
+   * .deny(2)
+   *
+   * @static
+   * @access                   public
+   * @description              Throws an IgnisError with status of 403.
+   * @param        {message}   Human-readable message to report.
+   * @param        {details}   Additional error details.
+   * @throws       {IgnisError}
+   */
+  static deny(message, details) {
+    details = _.assign(details || { }, { sensitive: true });
+    throw new IgnisError(403, message, details);
+  }
+
+  /**
+   * .notFound(2)
+   *
+   * @static
+   * @access                   public
+   * @description              Throws an IgnisError with status of 404.
+   * @param        {message}   Human-readable message to report.
+   * @param        {details}   Additional error details.
+   * @throws       {IgnisError}
+   */
+  static notFound(message, details) {
+    throw new IgnisError(404, message, details);
+  }
+
+}
--- src/ignis.js
+++ src/ignis.js
@@ -5,9 +5,25 @@
  * @license MIT
  */
 
+import FS           from 'fs';
+import YAML         from 'yamljs';
+
 import DataSource   from './data/source';
 import DataModel    from './data/model';
 
+
+/*!
+ * Patch require() to handle .yml/.yaml files.
+ */
+require.extensions['.yml'] = function(module, filename) {
+  return YAML.parse(FS.readFileSync(filename, 'utf8'));
+};
+require.extensions['.yaml'] = require.extensions['.yml'];
+
+
+/*!
+ * Export the root Ignis.js object.
+ */
 export default {
   data:   DataSource,
   model:  DataModel
