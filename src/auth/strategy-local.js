fc22c89a97304f8f5a3a7e3e605929fae081b279
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-08-21 01:31:07 -0400

    Added passport.js auth strategy wrappers

---
+++ src/auth/strategy-local.js
@@ -0,0 +1,34 @@
+/**
+ * auth/strategy-local.js
+ *
+ * @author  Denis Luchkin-Zhou <denis@ricepo.com>
+ * @license MIT
+ */
+
+import Bluebird    from 'bluebird';
+import Passport    from 'passport';
+import * as Local  from 'passport-local';
+
+
+/**
+ * local(2)
+ *
+ * @description    Specifies the callback function for verifying local login.
+ * @param          {options}   [Optional] Passport.js strategy options.
+ * @param          {callback}  Function that verifies the login.
+ */
+export default function local(callback) {
+  let options  = Object.create(null);
+  if (arguments.length === 2) {
+    options  = arguments[0];
+    callback = arguments[1];
+  }
+
+  let wrapper = function(username, password, done) {
+    return Bluebird
+      .try(() => { return callback(username, password); })
+      .nodeify(done);
+  };
+
+  Passport.use(new Local.Strategy(options, wrapper));
+}
